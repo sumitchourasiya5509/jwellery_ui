b81f2a020206095cae0707c4e939f049a128f56b
Sumit Chourasiya <sumitchourasiya5509@gmail.com>
2021-05-15 16:08:10 +0530

    latest changes

--- src/Bills.js
+++ src/Bills.js
@@ -68,14 +68,12 @@ function Bills() {
     }
 
     const handleSubmit = () => {
-     //console.log(data)
      let model = {
         "GoldPrice": parseFloat(goldPrice.goldPrice !== "" ? goldPrice.goldPrice : 0.0 ),
         "Discount": parseFloat(discount.discount !== "" ? discount.discount : 0.0 ),
         "Weight":  parseFloat(weight.weight !== "" ? weight.weight : 0.0 ),
         "TotalPrice": 0
     }
-    console.log(model)
        let url = `https://localhost:44306/api/bills/GetBill/`
        return fetch(url,{
         method: 'POST',
@@ -86,12 +84,18 @@ function Bills() {
         body: JSON.stringify(model)
         }).then(response => response.json())
             .then(bill =>{
-                console.log(bill)
                settotalprice(bill.totalPrice)
        })}
 
        return (
         <form style={formStyle} onSubmit={handleSubmit} >
+            <div className="row">
+                <div className="col-2"></div>
+                <div className="col-2"></div>
+                <div className="col float-right">
+                    <h5>Welcome {location.state.user.userName}</h5>
+                </div>
+            </div>
              <div className="row">
               <div className="col-4"><label style={labelStyle} >gold price (per Gram): </label></div>
               <div className="col"><input type="text" style={inputStyle} onChange={(e)=> goldPriceHandler(e)}/></div>
@@ -102,7 +106,7 @@ function Bills() {
             </div>
             <div className="row">
               <div className="col-4"><label style={labelStyle} >discount: </label></div>
-              <div className="col"><input type="text" style={inputStyle} onChange={(e)=> discountHandler(e)}/></div>
+              <div className="col"><input type="text" style={inputStyle} onChange={(e)=> discountHandler(e)} disabled={location.state.user.userType === "Regular"} value={discount}/></div>
             </div>
             <div className="row">
               <div className="col-4"><label style={labelStyle} >Total Price: </label></div>
